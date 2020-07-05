cc51f4a5660202b291456ae23b68494b975869ba
ZerLock <leo27.dubosclard@gmail.com>
2020-07-05 22:13:57 +0200

    code mofified

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define MAX  10000
+#define MIN 0
 
 int main(int argc, char *argv[]){
     int n = 10;
     int mailaddress[n];
     for(int x = 0; x < n; x++)
         mailaddress[x] = x;
-    int passmail[n] = {15, 188 ,15498 ,181 ,78487, 7848 ,165 ,17848 ,474 , 87};
+    int passmail[n];
+    passmail[0] = 474;
+    for(int a = 1; a < n; a++){
+        srand(time(NULL));
+        passmail[a] = (rand() % (MAX - MIN + 1)) + MIN;
+    }
     
     for(int i = 0; i < n; i++){
         for(int y = 0; y < n; y++){
-            if(mailaddress[i] == 3 && mailpass[y] == 474){
+            if(mailaddress[i] == 3 && passmail[y] == 474){
                 printf("%d : %d = true", mailaddress[i], passmail[y]);
             }else{
                 printf("%d : %d = false", mailaddress[i], passmail[y]);
             }
+            printf("\n");
         }
     }
     
