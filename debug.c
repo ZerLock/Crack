ad770a40566273a645e596747e93ff9fa904cf23
ZerLock <leo27.dubosclard@gmail.com>
2020-07-10 20:06:44 +0200

    MàJ

--- debug.c
+++ debug.c
@@ -7,6 +7,9 @@
 #define TAILLE_CHAINE 100
 
 int main(int argc, char *argv[]){
+    int n, x = 0, y = 0, u = 0;
+    scanf("%d", &n);
+
     char actualCaracter, mailaddress[TAILLE_CHAINE] = "", passmail[TAILLE_CHAINE] = "";
     
     FILE* dataFile = NULL;
@@ -15,10 +18,24 @@ int main(int argc, char *argv[]){
     
     if(dataFile != NULL){
         for(int i = 0; i < n; i++){
+            mailaddress[TAILLE_CHAINE] = "";
+            passmail[TAILLE_CHAINE] = "";
             do{
                 actualCaracter = fgetc(dataFile);
-                printf("%c", actualCaracter);
-            }while(actualCaracter != "\n");
+                if(x = 0){
+                    mailaddress[y] = actualCaracter;
+                    y++;
+                }else if(x = 1){
+                    passmail[u] = actualCaracter;
+                    u++;
+                }
+            }while(actualCaracter != " ");
+            x = 1;
+            if(mailaddress == "fjdfdhsj@sfr.fr" && passmail == "256df6"){
+                printf("%s : %s = true", mailaddress, passmail);
+            }else{
+                printf("%s : %s = false", mailaddress, passmail);
+            }
         }
     }else{
         printf("Erreur : Impossible d'ouvrir le fihcier data.txt !");
